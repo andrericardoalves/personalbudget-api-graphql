db3da5b2b57d00b93e11b75750e7f97db0ea5cf2
andrericardoalves <andrecajuru@gmail.com>
2022-01-02 21:32:45 +0000

    Add State endpoint

---
+++ src/main/java/com/alves/personalbudget/graphql/StateGraphQL.java
@@ -0,0 +1,20 @@
+package com.alves.personalbudget.graphql;
+
+import com.alves.personalbudget.model.State;
+import com.alves.personalbudget.service.StateService;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class StateGraphQL implements GraphQLQueryResolver {
+
+    @Autowired
+    private StateService service;
+
+    public List<State> findAllStates(){
+        return service.findAll();
+    }
+}
